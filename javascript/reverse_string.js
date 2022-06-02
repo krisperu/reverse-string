6ca5af5c25a798fe812ce1a4f5a8e079d25e03b6
Kristina Peru <krissy.germanov@gmail.com>
2022-06-02 13:22:38 -0600

    commit tree aba2bb2b406d6fda0e88afc46bfe5e8bd57c2e81

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,5 @@
 function reverseString(str) {
-  // type your code here
+  return str.split('').reverse().join('')
 }
 
 if (require.main === module) {
@@ -17,3 +17,8 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// First we split the string into single characters
+// Then we reverse what we split
+// Then we join it back together
+// Then we return the reversed string
